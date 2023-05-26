42206dd38a3a5ff80307e447d69553d4932df1db
arungane944 <134732953+arungane944@users.noreply.github.com>
2023-05-26 23:49:42 +0530

    initial commit

---
+++ monitor/src/main/java/com/springboot/monitor/monitor/MonitorApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.monitor.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+	@GetMapping("/monitor")
+	public String monitor() {
+		return "Hello World";
+	}
+
+}
